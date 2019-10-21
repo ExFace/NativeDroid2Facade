2ab1e27c8270b740f81fac8d855e03f5f188434b
tmc <tmc@NBDR211.salt-solutions.de>
2019-10-21 12:31:31 +0200

    FIX added description for nd2Button::buildHtmlHeadTags()

--- Facades/Elements/nd2Button.php
+++ Facades/Elements/nd2Button.php
@@ -73,6 +73,12 @@ class nd2Button extends nd2AbstractElement
         return $output;
     }
     
+
+    /**
+     * overwrites the standard includes and adds custom includes from JqueryButtonTrait::buildHtmlHeadTagsForCustomScriptIncludes()
+     * {@inheritDoc}
+     * @see \exface\Core\Facades\AbstractAjaxFacade\Elements\AbstractJqueryElement::buildHtmlHeadTags()
+     */
     public function buildHtmlHeadTags()
     {
         $tags = parent::buildHtmlHeadTags();
